50adc25a089e7a7d407cf4b05dc521916029288c
Robert Rousset <whyisrob@hotmail.com>
2021-11-02 15:26:40 +0800

    added a redirect to change path to /signin if not signed in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+
 import Dashboard from "./componenets/Dashboard";
 import Signin from "./componenets/Signin";
 
 function App() {
-  let signedIn = true;
-  return <>{signedIn ? <Dashboard /> : <Signin />}</>;
+  let signedIn = false;
+
+  return (
+    <Router>
+      {signedIn ? (
+        <Route exact path="/dashboard" component={Dashboard} />
+      ) : (
+        <>
+          <Redirect to="/signin" />{" "}
+          <Route exact path="/signin" component={Signin} />
+        </>
+      )}
+    </Router>
+  );
 }
 
 export default App;
