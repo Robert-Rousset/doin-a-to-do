c2ab6fdc305efda80a1d90845e7c4dc73ff20794
Robert Rousset <whyisrob@hotmail.com>
2021-11-04 14:37:13 +0800

    fixing problems with github deployment

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
       <Router>
         {signedIn ? (
           <>
-            <Redirect to="/dashboard" />
-            <Route exact path="/dashboard" component={Dashboard} />
+            <Redirect to="/doin-a-to-do/dashboard" />
+            <Route path="/doin-a-to-do/dashboard" component={Dashboard} />
           </>
         ) : (
           <>
-            <Redirect to="/signin" />{" "}
-            <Route exact path="/signin" component={SignIn} />
+            <Redirect to="/doin-a-to-do/signin" />{" "}
+            <Route exact path="/doin-a-to-do/signin" component={SignIn} />
           </>
         )}
       </Router>
